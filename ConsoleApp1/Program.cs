7d5ddec003c8eb89c5a63128af6e99e718470696
Hunghh90 <hungbkvn9x1@gmail.com>
2022-12-24 20:51:10 +0700

    upcode

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,35 @@
 ﻿using ConsoleApp1.session1;
 using ConsoleApp1.session2;
+using ConsoleApp1.session3;
 
 public class Program
-{ static void Main(String[] args)
+{
+    static void Main(String[] args)
+    {
+        try 
+        {
+            int x = 10;
+            int y = 5;
+            if(y == 0) 
+            {
+                throw new Exception("Y bang 0 mat roi");
+            }else
+            {
+                float z = x / y;
+                Console.WriteLine("z =" + z);
+            }
+            
+           
+        }catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally 
+        {
+
+        }
+    }
+    static void Main2(String[] args)
     {
         Human h = new Human();
         h.Run();
